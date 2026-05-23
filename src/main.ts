7b1d34374d4e78c588bdf190bbe2fb1249da576d
Thomas Edson <andepipous@gmail.com>
2026-05-23 17:24:53 -0300

    feat: define initial scss and tailwind structure

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  logoFacebook, 
+  logoGoogle, 
+  logoTwitter, 
+  eyeOutline, 
+  eyeOffOutline, 
+  checkmarkCircleOutline, 
+  lockOpenOutline,
+  checkmarkCircle,
+} from 'ionicons/icons';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+addIcons({
+  logoFacebook,
+  logoGoogle,
+  logoTwitter,
+  eyeOutline,
+  eyeOffOutline,
+  checkmarkCircleOutline,
+  checkmarkCircle,
+  lockOpenOutline
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
